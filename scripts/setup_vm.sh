aabd5d54aecf17b2ac01b9b2d4f5c6fe6e97dc90
Jeffrey Smith <jsmith@SoftEng.(none)>
2014-11-03 16:37:21 -1000

    Fixed issue with script failing due to rvm GPG signature verification failure.  See https://www.digitalocean.com/community/questions/curl-l-get-rvm-io-bash-s-stable-fails-on-cent-os-on-hostgator.

--- scripts/setup_vm.sh
+++ scripts/setup_vm.sh
@@ -3,9 +3,10 @@
 set -e
 
 # install curl
-apt-get install curl -y
+apt-get install curl gnupg2 -y
 
 # install rvm 
+gpg2 --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 curl -L https://get.rvm.io | bash -s stable
 
 # activate the correct rvm environment
